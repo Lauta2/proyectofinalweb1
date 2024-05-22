a9600ebc33cdd48b7b7f90f76991b8e3685ae7c5
Lauta2 <163307124+Lauta2@users.noreply.github.com>
2024-05-21 22:29:07 -0300

    Carrusel de la pagina 2

--- carrusel1.js
+++ carrusel1.js
@@ -19,3 +19,24 @@ let array=["imgcarru/River_Plate.jpg","imgcarru/Racing.jpg","imgcarru/Argentinos
         }
         im.src = array[cont];
     }   
+let array1=["imgcarru2/Di Maria.jpg","imgcarru2/Fernando Redondo.jpg","imgcarru2/Messi.jpg","imgcarru2/Tevez.jpg","imgcarru2/Zanetti.jpeg"];
+let im1 = document.getElementById("foto1");
+let cont1 = 0;
+
+function anterior1(){
+    if(cont1===0){
+        cont1 = array1.length -1;
+    }else{
+        cont1--;
+    }
+    im1.src = array1[cont1];
+}
+
+function siguiente1(){
+    if(cont1===array1.length -1){
+        cont1 =0;
+    }else{
+        cont1++;
+    }
+    im1.src = array1[cont1];
+}   
